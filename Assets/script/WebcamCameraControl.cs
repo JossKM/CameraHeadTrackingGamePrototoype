d5a5dc1d17aefbe77f031a5e44b6a8403aa6fdbe
JossKM <jossmooyoung@gmail.com>
2021-11-11 13:35:27 -0500

    Experimenting with just taking position data straight from AITrack. For some reason position numbers keep flipping?

--- Assets/script/WebcamCameraControl.cs
+++ Assets/script/WebcamCameraControl.cs
@@ -20,12 +20,19 @@ public class WebcamCameraControl : MonoBehaviour
 
     public float camScale = 0.01f;
 
-    public double ry = 0; //rotate angle on Y axis
-    public double rx = 0; //rotate angle on X axis
-    public double rz = 0;
-    public double pX = 0;
-    public double pY = 0;
-    public double pZ = 0;
+    public float pX = 0;
+    public float pY = 0;
+    public float pZ = 0;
+    public float ry = 0; //rotate angle on Y axis
+    public float rx = 0; //rotate angle on X axis
+    public float rz = 0;
+
+    public float pXOffset = 0;
+    public float pYOffset = 0;
+    public float pZOffset = 0;
+    public float ryOffset = 0;
+    public float rxOffset = 0;
+    public float rzOffset = 0;
     // 2. InitUDP
 
     private void InitUDP()
@@ -48,12 +55,34 @@ public class WebcamCameraControl : MonoBehaviour
                 byte[] bytes = client.Receive(ref anyIP);                               // receive data from port
                                                                                         //rZ rY rX
 
-                pX = BitConverter.ToDouble(bytes, 0);
-                pY = BitConverter.ToDouble(bytes, 8);
-                pZ = BitConverter.ToDouble(bytes, 16);
-                ry = BitConverter.ToDouble(bytes, 24);
-                rx = BitConverter.ToDouble(bytes, 32);
-                rz = BitConverter.ToDouble(bytes, 40);
+                //pX = BitConverter.ToDouble(bytes, 0);
+                //pY = BitConverter.ToDouble(bytes, 8);
+                //pZ = BitConverter.ToDouble(bytes, 16);
+                //ry = BitConverter.ToDouble(bytes, 24);
+                //rx = BitConverter.ToDouble(bytes, 32);
+                //rz = BitConverter.ToDouble(bytes, 40);
+
+                //ry = 360.0f + (float)BitConverter.ToDouble(bytes, 0);
+                //rx = 360.0f + -(float)BitConverter.ToDouble(bytes, 8);
+                //rz = 360.0f + 0;//(float)BitConverter.ToDouble(bytes, 16);
+                //
+                //ry = Mathf.Repeat(ry, 360.0f);
+                //rx = Mathf.Repeat(rx, 360.0f);
+                //rz = Mathf.Repeat(rz, 360.0f);
+
+                pX = -(float)BitConverter.ToDouble(bytes, 0);
+                pY = -(float)BitConverter.ToDouble(bytes, 8);
+                pZ = (float)BitConverter.ToDouble(bytes, 16);
+
+                // Apply offsets
+                pX += pXOffset;
+                pY += pYOffset;
+                pZ += pZOffset;
+                ry += ryOffset;
+                rx += rxOffset;
+                rz += rzOffset;
+
+                //Ensure -60 is the same as 30
             }
             catch (Exception e)
             {
@@ -72,15 +101,34 @@ public class WebcamCameraControl : MonoBehaviour
         ry = 0;                                                                         // init rotate angle on Y axis 
         rx = 0;                                                                         // init rotate angle on X axis 
         rz = 0;
+        pY = 0;
+        pX = 0;
+        pZ = 0;
+
         InitUDP();                                                                      // init UDP
     }
 
-    // 5. Update position of avatar
+    void SetZeroes()
+    {
+        pXOffset = -pX;
+        pYOffset = -pY;
+        pZOffset = -pZ;
+        ryOffset = -ry;
+        rxOffset = -rx;
+        rzOffset = -rz;
+    }
 
+    // 5. Update position of avatar
     void Update()
     {
-        Quaternion rot = baseTransform.rotation * Quaternion.Euler((float)rx, (float)ry, (float)rz);   // control avatar with rotation angle on X,Y axis
-        av.rotation = Quaternion.Lerp(av.rotation, rot, 0.05f);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SetZeroes();
+        }
+
+
+        Quaternion rot = Quaternion.Euler((float)rx, (float)ry, (float)rz);   // control avatar with rotation angle on X,Y axis
+        av.rotation = rot;//Quaternion.Lerp(av.rotation, rot, 0.05f);
         av.position = baseTransform.MultiplyVector(new Vector3((float)pX * camScale, (float)pY * camScale, (float)pZ * camScale));
     }
 }
